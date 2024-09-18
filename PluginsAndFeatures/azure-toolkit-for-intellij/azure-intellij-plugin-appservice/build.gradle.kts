45c889e079720063ee0a4b0c6ff95e5ffc65f587
Rival Abdrakhmanov <rival.abdrakhmanov@gmail.com>
2024-09-18 11:26:51 +0200

    Move org.codehaus.plexus:plexus-archiver

--- PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-appservice/build.gradle.kts
+++ PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-appservice/build.gradle.kts
@@ -37,7 +37,7 @@ dependencies {
     implementation(libs.azureToolkitIdeAppserviceLib)
     implementation(libs.azureToolkitIdeContainerregistryLib)
     implementation("com.jcraft:jsch:0.1.55")
-    implementation("org.codehaus.plexus:plexus-archiver:4.2.7")
+    implementation(libs.plexusArchiver)
     implementation("org.codehaus.plexus:plexus-container-default:2.1.1")
     implementation("com.neovisionaries:nv-websocket-client:2.14")
 
