6a3732ef6953d27a96ba48e501c496fc6cea261c
Rival Abdrakhmanov <rival.abdrakhmanov@gmail.com>
2024-10-01 15:38:27 +0200

    Use CertificateManager as the trustManager and migrate to kotlinx.serialization

--- PluginsAndFeatures/azure-toolkit-for-rider/azure-intellij-plugin-appservice-dotnet/src/main/kotlin/com/microsoft/azure/toolkit/intellij/legacy/function/coreTools/FunctionCoreToolsReleaseFeedService.kt
+++ PluginsAndFeatures/azure-toolkit-for-rider/azure-intellij-plugin-appservice-dotnet/src/main/kotlin/com/microsoft/azure/toolkit/intellij/legacy/function/coreTools/FunctionCoreToolsReleaseFeedService.kt
@@ -2,17 +2,24 @@
  * Copyright 2018-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the MIT license.
  */
 
+@file:OptIn(ExperimentalSerializationApi::class)
+
 package com.microsoft.azure.toolkit.intellij.legacy.function.coreTools
 
-import com.google.gson.GsonBuilder
-import com.google.gson.annotations.SerializedName
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.service
+import com.intellij.util.net.ssl.CertificateManager
 import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
 
 @Service(Service.Level.APP)
 class FunctionCoreToolsReleaseFeedService : Disposable {
@@ -20,50 +27,66 @@ class FunctionCoreToolsReleaseFeedService : Disposable {
         fun getInstance(): FunctionCoreToolsReleaseFeedService = service()
     }
 
-    private val client = HttpClient(CIO)
+    private val client = HttpClient(CIO) {
+        engine {
+            https {
+                trustManager = CertificateManager.getInstance().trustManager
+            }
+        }
+        install(ContentNegotiation) {
+            json(Json {
+                explicitNulls = false
+                ignoreUnknownKeys = true
+            })
+        }
+    }
 
     suspend fun getReleaseFeed(feedUrl: String): ReleaseFeed {
-        val body: String = client.get(feedUrl).body()
-        return GsonBuilder().create().fromJson(body, ReleaseFeed::class.java)
+        val response = client.get(feedUrl)
+        return response.body<ReleaseFeed>()
     }
 
     override fun dispose() = client.close()
 }
 
+@Serializable
 data class ReleaseFeed(
-    @SerializedName("tags")
+    @SerialName("tags")
     val tags: Map<String, Tag>,
-    @SerializedName("releases")
+    @SerialName("releases")
     val releases: Map<String, Release>
 )
 
+@Serializable
 data class Tag(
-    @SerializedName("release")
+    @SerialName("release")
     val release: String?,
-    @SerializedName("releaseQuality")
+    @SerialName("releaseQuality")
     val releaseQuality: String?,
-    @SerializedName("hidden")
+    @SerialName("hidden")
     val hidden: Boolean
 )
 
+@Serializable
 data class Release(
-    @SerializedName("templates")
+    @SerialName("templates")
     val templates: String?,
-    @SerializedName("coreTools")
+    @SerialName("coreTools")
     val coreTools: List<ReleaseCoreTool>
 )
 
+@Serializable
 data class ReleaseCoreTool(
-    @SerializedName("OS")
+    @SerialName("OS")
     val os: String?,
-    @SerializedName("Architecture")
+    @SerialName("Architecture")
     val architecture: String?,
-    @SerializedName("downloadLink")
+    @SerialName("downloadLink")
     val downloadLink: String?,
-    @SerializedName("sha2")
+    @SerialName("sha2")
     val sha2: String?,
-    @SerializedName("size")
+    @SerialName("size")
     val size: String?,
-    @SerializedName("default")
+    @SerialName("default")
     val default: Boolean
 )
