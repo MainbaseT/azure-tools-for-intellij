70bc1433728e73d2856913136e8aeb4870a98b27
Rival Abdrakhmanov <rival.abdrakhmanov@jetbrains.com>
2026-05-04 14:43:29 +0200

    Remove azure-intellij-plugin-appservice-dotnet-aspire module

--- PluginsAndFeatures/azure-toolkit-for-rider/build.gradle.kts
+++ PluginsAndFeatures/azure-toolkit-for-rider/build.gradle.kts
@@ -68,7 +68,6 @@ dependencies {
     implementation(project(path = ":azure-intellij-plugin-monitor"))
     implementation(project(path = ":azure-intellij-plugin-appservice"))
     implementation(project(path = ":azure-intellij-plugin-appservice-dotnet"))
-    implementation(project(path = ":azure-intellij-plugin-appservice-dotnet-aspire"))
     implementation(project(path = ":azure-intellij-plugin-database"))
     implementation(project(path = ":azure-intellij-plugin-database-dotnet"))
     implementation(project(path = ":azure-intellij-plugin-cloud-shell"))
