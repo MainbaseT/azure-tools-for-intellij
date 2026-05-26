bcd5d8e3808f610fd1a718a5c7e9eadd6759da36
Rival Abdrakhmanov <rival.abdrakhmanov@jetbrains.com>
2026-05-26 12:48:28 +0200

    RIDER-137337 remove `FunctionLocalSettings` and use platform's `AzureFunctionsLocalSettings`

--- PluginsAndFeatures/azure-toolkit-for-rider/azure-intellij-plugin-appservice-dotnet/src/main/kotlin/com/microsoft/azure/toolkit/intellij/legacy/function/FunctionUtils.kt
+++ PluginsAndFeatures/azure-toolkit-for-rider/azure-intellij-plugin-appservice-dotnet/src/main/kotlin/com/microsoft/azure/toolkit/intellij/legacy/function/FunctionUtils.kt
@@ -4,9 +4,6 @@
 
 package com.microsoft.azure.toolkit.intellij.legacy.function
 
-// Known and supported list of tags from https://github.com/Azure/azure-functions-tooling-feed/blob/main/cli-feed-v4.json
-val FUNCTIONS_CORE_TOOLS_KNOWN_SUPPORTED_VERSIONS = listOf("v2", "v3", "v4")
-
 // Latest supported version by the Azure Toolkit for Rider
 const val FUNCTIONS_CORE_TOOLS_LATEST_SUPPORTED_VERSION = "v4"
 
