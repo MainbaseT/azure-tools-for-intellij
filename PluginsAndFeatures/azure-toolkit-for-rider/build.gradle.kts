2bc770ddb2a1410f1326fb67e365df131e621eb9
Rival Abdrakhmanov <rival.abdrakhmanov@jetbrains.com>
2026-06-02 13:19:50 +0200

    Remove exclusion of `groovy-templates` dependency from build files

--- PluginsAndFeatures/azure-toolkit-for-rider/build.gradle.kts
+++ PluginsAndFeatures/azure-toolkit-for-rider/build.gradle.kts
@@ -93,7 +93,6 @@ configurations {
     implementation { exclude(module = "log4j") }
     implementation { exclude(module = "stax-api") }
     implementation { exclude(module = "groovy-xml") }
-    implementation { exclude(module = "groovy-templates") }
     implementation { exclude(module = "jna") }
     implementation { exclude(module = "xpp3") }
     implementation { exclude(module = "pull-parser") }
